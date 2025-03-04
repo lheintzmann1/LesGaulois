ee2e83120d503a5e8c8c3774d1cb1c8a51be8e03
Lucas <lheintzmann@disroot.org>
2025-03-04 09:25:13 +0100

    Invariant pre/post-conditions

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -7,6 +7,7 @@ public class Romain {
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
+		assert force > 0 : "La force doit être supérieure à 0";
 	}
 	
 	public String getNom() {
@@ -22,13 +23,19 @@ public class Romain {
 	}
 	
 	public void recevoirCoup(int forceCoup) {
-		this.force = this.force - forceCoup;
-		if (this.force <= 0) {
-			System.out.println(prendreParole() + "J'abandonne !");
+		assert forceCoup > 0 : "La force du coup doit être supérieure à 0";
+		int oldforce = force;
+		force -= forceCoup;
+		if (force <= 0) {
+			parler("J'abandonne !");
 		} else {
-			System.out.println(prendreParole() + "Aïe");
+			parler("Aïe");
 		}
-		
+		assert force < oldforce : "La force n'a pas diminué";		
+	}
+
+	public static void main(String[] args) {
+		Romain minus = new Romain("Minus", 6);
 	}
 	
 }
