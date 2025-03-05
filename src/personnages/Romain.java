b4c0e08eeb67ec52625d530aff678bd2efad0b9a
Lucas <lheintzmann@disroot.org>
2025-03-05 07:37:41 +0100

    Enums

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -2,6 +2,8 @@ package personnages;
 
 public class Romain {
 	private String nom;
+	private Equipement[] equipements = new Equipement[2];
+	private int nbEquipements = 0;
 	private int force;
 	
 	public Romain(String nom, int force) {
@@ -32,6 +34,34 @@ public class Romain {
 			parler("Aïe");
 		}
 		assert force < oldforce : "La force n'a pas diminué";		
+	}
+
+	public void ajouterEquipement(Equipement equipement) {
+		equipements[nbEquipements] = equipement;
+		nbEquipements ++;
+		texteEquipementSoldat(" s'équipe avec un " + equipement.toString());
+	}
+
+	public void texteEquipementSoldat(String texte) {
+		parler("Le soldat " + nom + texte);
+	}
+
+	public void sEquiper (Equipement equipement) {
+		switch (nbEquipements) {
+			case 0:
+				ajouterEquipement(equipement);
+				break;
+			case 1:
+				if (equipements[0] == equipement) {
+					texteEquipementSoldat(" possède déjà un " + equipement.toString());
+				} else {
+					ajouterEquipement(equipement);
+				}
+				break;
+			case 2:
+				texteEquipementSoldat(" est deja bien protégé !");
+				break;
+		}
 	}
 
 	public static void main(String[] args) {
